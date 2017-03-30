da2d751c843ee0ca8bae821077fb57cddedd4b67
Nikola Zeravcic <niks986@gmail.com>
2017-03-30 23:58:47 +0200

    Base start point
    Developing admin functionality

---
+++ src/resources/views/auth/emails/password.blade.php
@@ -0,0 +1,1 @@
+{{ trans('bytenet::base.click_here_to_reset') }}: <a href="{{ $link = url('/admin/password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
